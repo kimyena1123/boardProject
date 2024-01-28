c66283a9c01562293999accd70b2ed08bde758b1
kimyena1123 <kyq104@gmail.com>
2024-01-29 03:06:53 +0900

    #11 - repository interface 작성
    이걸로 db에 접근

---
+++ fastcampus-project-board/src/main/java/com/fastcampus/projectboard/repository/ArticleRepository.java
@@ -0,0 +1,9 @@
+package com.fastcampus.projectboard.repository;
+
+import com.fastcampus.projectboard.domain.Article;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+//@Repository
+public interface ArticleRepository extends JpaRepository<Article, Long> {
+}
