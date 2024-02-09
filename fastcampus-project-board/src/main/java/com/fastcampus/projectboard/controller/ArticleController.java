c5698d9c7e46361843104054edbb0dc43a6527ef
kimyena1123 <kyq104@gmail.com>
2024-02-09 18:49:54 +0900

    #17 - freemarker를 사용한 controller
    FreemarkerController는 freemarker가 잘 적용되는지 확인하기 위한 예비 controller이다.
    잘 작동하기에 ArticleController에 적용

--- fastcampus-project-board/src/main/java/com/fastcampus/projectboard/controller/ArticleController.java
+++ fastcampus-project-board/src/main/java/com/fastcampus/projectboard/controller/ArticleController.java
@@ -3,10 +3,13 @@ package com.fastcampus.projectboard.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
+import java.util.Map;
+import java.util.List;
+
 
 /*
 /articles
@@ -18,9 +21,26 @@ import java.util.List;
 @Controller
 public class ArticleController {
 
+//    @GetMapping //localhost:8080/articles
+//    public String articles(Map model){
+//        model.put("articles", List.of());
+//        return "articles/index";
+//    }
+
     @GetMapping
-    public String articles(ModelMap map) {
-        map.addAttribute("articles", List.of());
+    public String articles(ModelMap map){
+        map.put("articles", List.of());
         return "articles/index";
     }
+
+    @GetMapping("/{articleId}")
+    public String article(@PathVariable(name = "articleId") Long articleId, ModelMap map) {
+        map.addAttribute("article", null);
+        map.addAttribute("articleComment", List.of());
+
+        return "articles/detail";
+    }
+
+
+
 }
